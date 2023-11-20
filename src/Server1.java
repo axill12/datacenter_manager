a1dfdb56ecff9415f9ce5858cd9bbc71ffa471b4
axill12 <axilleas12@gmail.com>
2023-11-20 18:07:15 +0200

    I completed Worker class in Server1

--- src/Server1.java
+++ src/Server1.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,6 +18,7 @@ public class Server1 {
         }
     }
 
+    //The purpose of this class is to do just some calculations. They are meaningless. The purpose is to make processor work.
     public static class Worker implements Runnable {
 
         private Socket client;
@@ -25,7 +28,26 @@ public class Server1 {
         }
 
         public void run () {
-
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+                String line;
+                line = reader.readLine();
+                int timesOfAddition = -1;
+                try {
+                    timesOfAddition = Integer.parseInt(line);
+                } catch (NumberFormatException e) {
+                    System.out.println ("The input from client isn't in a form that can be considered integer. This is input;\n" + line);
+                }
+                int result = 0;
+                //It adds timesOfAddition * 10 times 1 to result.
+                if (timesOfAddition != -1) {
+                    for (int i=0; i<timesOfAddition*10; i++) {
+                        result++;
+                    }
+                }
+                System.out.println(result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
