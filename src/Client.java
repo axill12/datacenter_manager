e78fb42708da909e522eed33bc0da580799f8da7
axill12 <axilleas12@gmail.com>
2023-12-22 18:54:48 +0200

    I progressed in WorkScheduler

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -6,8 +8,22 @@ import java.net.UnknownHostException;
 public class Client {
 
     public static void main (String args []) {
-        new Thread(new Worker()).start();
-        new Thread(new Worker()).start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                String userInput = reader.readLine();
+                if (userInput.equals("1")) {
+                    new Thread(new Worker()).start();
+                } else if (userInput.equals("2")) {
+                    new Thread(new Worker()).start();
+                    new Thread(new Worker()).start();
+                } else if (userInput.equals("ex")) {
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static class Worker implements Runnable {
