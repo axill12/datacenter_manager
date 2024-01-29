def2e2968872af9fa877048a8129ce834fb6aa01
axill12 <axilleas12@gmail.com>
2024-01-29 15:00:20 +0200

    I fixed the minor issue in ContainerManager of assigning rarely one extra token to applications.

--- src/ContainerManager.java
+++ src/ContainerManager.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,6 +26,8 @@ public class ContainerManager  {
         int SLO;
         int necessaryTokens;
 
+        List<ServerInformations> listOfServers = new ArrayList<>();
+
         try (ServerSocket server = new ServerSocket(7170)) {
             while (true) {
                 receiver = server.accept();
@@ -35,10 +39,18 @@ public class ContainerManager  {
                     System.out.println ("The id of server user provided does not contains only digits or contains nothing. His demand cannot be served.");
                     continue;
                 }
-                SLO = Integer.parseInt(reader.readLine());
-                necessaryTokens = (int) ((10.0 / SLO) * 50) + 1;
 
-                //TODO na luso to mikro problhma logo toy opoioy anatithetai ena token parapano otan (10.0 / SLO) * 50 tha htan akeraios an anti na ton ekteleso sthn java ektelousa ton upologismo se kompiouteraki. p.x. gia SLO = 20
+                SLO = Integer.parseInt(reader.readLine());
+                /*For a few values of SLO (10.0 / SLO) * 50 computes the equivalent of (int) ((10.0 / SLO) * 50) * 1.0,
+                  meaning (10.0 / SLO) * 50 does not have values like 25.04 or 25.3.
+                  Though if necessaryTokens are assigned always the value of (int) ((10.0 / SLO) * 50) + 1 in above case it takes value higher by 1 than it should have
+                  e.g. for SLO = 20 instead of 25 it is assigned 26, because of + 1.
+                */
+                if ((10.0 / SLO) * 50 == (int) ((10.0 / SLO) * 50) * 1.0) {
+                    necessaryTokens = (int) ((10.0 / SLO) * 50);
+                } else {
+                    necessaryTokens = (int) ((10.0 / SLO) * 50) + 1;
+                }
 
                 //It is better applications to be able to serve 7 requests simultaneously. That is the reason WorkScheduler.getTotalAvailableTokens() >= 7 is applied.
                 if (necessaryTokens <= WorkScheduler.getTotalAvailableTokens() && WorkScheduler.getTotalAvailableTokens() >= 7) {
@@ -52,6 +64,8 @@ public class ContainerManager  {
                     System.out.println("totalAvailableTokens: " + WorkScheduler.getTotalAvailableTokens());
 
                     //TODO na topothetei thn efarmogh sto buckets[serverCell]
+                    ServerInformations info = new ServerInformations(1, 2);
+
 
                 } else {
                     System.out.println("This server does not have enough tokens to serve this container suitably.");
@@ -67,6 +81,18 @@ public class ContainerManager  {
             System.out.println("User provided SLO, which is not in number format.");
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
+        }
+    }
+
+    //Holds the id of each server, which is placed in real computer and its position in buckets array of WorkScheduler.
+    private static class ServerInformations {
+        private int idOfServer;
+
+        private int positionOfServer;
+
+        private ServerInformations (int idOfServer, int positionOfServer) {
+            this.idOfServer = idOfServer;
+            this.positionOfServer = positionOfServer;
         }
     }
 
