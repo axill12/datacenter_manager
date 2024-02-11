ebd3267eab88d362fbdc385672fa88687b22ad0b
axill12 <axilleas12@gmail.com>
2024-02-11 19:35:54 +0200

    ContainerInstaller.sh can run containers with correct port with parameter more than 6.

--- ContainerInstaller.sh
+++ ContainerInstaller.sh
@@ -1,2 +1,2 @@
 docker pull axill12/applications_of_datacenter:server$1
-docker run --name server$1_container -p 683$(($1 + 3)):683$(($1 + 3)) -d axill12/applications_of_datacenter:server$1
+docker run --name server$1_container -p $((6833 + $1)):$((6833 + $1)) -d axill12/applications_of_datacenter:server$1
