9e00381bc21581dccb2632e4b9e980a28e9ac708
Achilleas Kakavas <61993544+axill12@users.noreply.github.com>
2024-04-23 21:16:15 +0300

    h

--- add_permissions.sh
+++ add_permissions.sh
@@ -1,4 +1,4 @@
-sudo chmod +x /var/lib/docker
+sudo chmod +x /var/lib/docker;
 sudo chmod +x /var/lib/docker/containers
 sudo chmod +x /var/lib/docker/image/overlay2/imagedb/content/sha256
 sudo chmod +x /var/lib/docker/image/overlay2/imagedb/content/sha256/117cf7bb4b159c7c54bc82cdf6ae6d2227324f57c5c2e0b1300f282fdbcb4a03
